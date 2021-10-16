da01d48e68b765d29288734546a5b54cb121cb93
Adam Łuszcz <adam.luszcz20@gmail.com>
2021-10-16 19:30:35 +0200

    Cleaned up ZooAplication

--- src/main/java/pl/pjatk/zoo/ZooApplication.java
+++ src/main/java/pl/pjatk/zoo/ZooApplication.java
@@ -3,26 +3,11 @@ package pl.pjatk.zoo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @SpringBootApplication
 public class ZooApplication {
 
 	public static void main(String[] args) {
-
-		//Klasa Zoo
-		//Zoo -> id, name, location, isClosed, list Animal
-		//Klasa Animal
-		//Animal -> id, specimen, diet (meat, vege, mixed), type (water, land, air), hunger, health
-
 		SpringApplication.run(ZooApplication.class, args);
-
-		Animal lion = new Animal(1, "lion", Diet.MEAT, Type.LAND, true, 100);
-
-		Zoo zoo = new Zoo(1, "Zoo", "Gdansk", false, List.of(lion));
-
-		System.out.println(zoo);
 	}
 
 }

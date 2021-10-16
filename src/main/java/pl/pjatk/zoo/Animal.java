5f6838b5f213a3c6e1a6fb765ef33c5777a9828b
Adam Łuszcz <adam.luszcz20@gmail.com>
2021-10-16 18:41:25 +0200

    Initial commit

---
+++ src/main/java/pl/pjatk/zoo/Animal.java
@@ -0,0 +1,67 @@
+package pl.pjatk.zoo;
+
+public class Animal {
+    private Integer id;
+    private String specimen;
+    private Diet diet;
+    private Type type;
+    private boolean isHungry;
+    private int health;
+
+    public Animal(Integer id, String specimen, Diet diet, Type type, boolean isHungry, int health) {
+        this.id = id;
+        this.specimen = specimen;
+        this.diet = diet;
+        this.type = type;
+        this.isHungry = isHungry;
+        this.health = health;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSpecimen() {
+        return specimen;
+    }
+
+    public void setSpecimen(String specimen) {
+        this.specimen = specimen;
+    }
+
+    public Diet getDiet() {
+        return diet;
+    }
+
+    public void setDiet(Diet diet) {
+        this.diet = diet;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public boolean isHungry() {
+        return isHungry;
+    }
+
+    public void setHungry(boolean hungry) {
+        isHungry = hungry;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+}
---
+++ src/main/java/pl/pjatk/zoo/Zoo.java
@@ -0,0 +1,58 @@
+package pl.pjatk.zoo;
+import java.util.List;
+
+public class Zoo {
+    private Integer id;
+    private String name;
+    private String location;
+    private boolean isClosed;
+    private List<Animal> animal;
+
+    public Zoo(Integer id, String name, String location, boolean isClosed, List<Animal> animal) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.isClosed = isClosed;
+        this.animal = animal;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public boolean isClosed() {
+        return isClosed;
+    }
+
+    public void setClosed(boolean closed) {
+        isClosed = closed;
+    }
+
+    public List<Animal> getAnimal() {
+        return animal;
+    }
+
+    public void setAnimal(List<Animal> animal) {
+        this.animal = animal;
+    }
+}
